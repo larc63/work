3526e905d861878d0bb5ec55ac59f2a98dd39d96
Luis Antonio <larc63@gmail.com>
2014-01-08 18:03:41 -0600

    Adding missing files

---
+++ TunnelInvader/TunnelInvaderTests/TunnelInvaderTests.m
@@ -0,0 +1,34 @@
+//
+//  TunnelInvaderTests.m
+//  TunnelInvaderTests
+//
+//  Created by Luis Antonio Rodriguez on 1/8/14.
+//  Copyright (c) 2014 Luis Antonio Rodriguez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TunnelInvaderTests : XCTestCase
+
+@end
+
+@implementation TunnelInvaderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
