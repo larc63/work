a253e16cf48779a20b32cbaa3c5cd299982a856f
Luis Antonio <larc63@gmail.com>
2015-08-22 23:09:42 -0500

    Added image resources for roshambo

---
+++ Udacity/iOSNanoDegree/04 Roshambo/Roshambo/MatchResultController.swift
@@ -0,0 +1,18 @@
+//
+//  MatchResultController.swift
+//  Roshambo
+//
+//  Created by Luis Antonio Rodriguez on 8/22/15.
+//  Copyright (c) 2015 Luis Antonio Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class MatchResultController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
