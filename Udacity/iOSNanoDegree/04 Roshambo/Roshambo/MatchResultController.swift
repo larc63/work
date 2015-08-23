d94b199242017153455fe804e8e62c8894f2a66f
Luis Antonio <larc63@gmail.com>
2015-08-23 12:59:31 -0500

    Implemented Roshambo functionality

--- Udacity/iOSNanoDegree/04 Roshambo/Roshambo/MatchResultController.swift
+++ Udacity/iOSNanoDegree/04 Roshambo/Roshambo/MatchResultController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class MatchResultController: UIViewController {
-    
+    var result = (RoshamboResults.itsATie, "It's a tie!")
+    @IBOutlet weak var theImage: UIImageView!
+    @IBOutlet weak var theLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        switch (result.0){
+        case .rockCrushesScissors:
+            theImage.image = UIImage(named: "RockCrushesScissors")
+            break
+        case .paperCoversRock:
+            theImage.image = UIImage(named: "PaperCoversRock")
+            break
+        case .scissorsCutPaper:
+            theImage.image = UIImage(named: "ScissorsCutPaper")
+            break
+        case .itsATie:
+            theImage.image = UIImage(named: "itsATie")
+            break
+        }
+        theLabel.text = String(result.1)
     }
-    
+    @IBAction func dismiss(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
--- Udacity/iOSNanoDegree/04 Roshambo/Roshambo/ViewController.swift
+++ Udacity/iOSNanoDegree/04 Roshambo/Roshambo/ViewController.swift
@@ -9,7 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var tempResult: (RoshamboResults, String)?
+    func getMatchResult(userPlay:RoshamboPlays) -> (RoshamboResults, String){
+        // Generate a random Int32 using arc4Random
+        var result = RoshamboResults.itsATie
+        var message = "It's a tie!"
+        let computerPlay = RoshamboPlays(rawValue: Int(1 + arc4random() % 3))!
+        println("computer plays \(computerPlay.rawValue)")
+        switch (userPlay){
+        case .ROCK:
+            switch (computerPlay){
+            case .ROCK:
+                result = RoshamboResults.itsATie
+                message = "It's a tie!"
+                break
+            case .PAPER:
+                result = RoshamboResults.paperCoversRock
+                message = "Computer wins!"
+                break
+            case .SCISSORS:
+                result = RoshamboResults.rockCrushesScissors
+                message = "You win!"
+                break
+            }
+            break
+        case .SCISSORS:
+            switch (computerPlay){
+            case .ROCK:
+                result = RoshamboResults.rockCrushesScissors
+                message = "Computer wins!"
+                break
+            case .PAPER:
+                result = RoshamboResults.scissorsCutPaper
+                message = "You win!"
+                break
+            case .SCISSORS:
+                result = RoshamboResults.itsATie
+                message = "It's a tie!"
+                break
+            }
+            break
+        case .PAPER:
+            switch (computerPlay){
+            case .ROCK:
+                result = RoshamboResults.paperCoversRock
+                message = "You win!"
+                break
+            case .PAPER:
+                result = RoshamboResults.itsATie
+                message = "It's a tie!"
+                break
+            case .SCISSORS:
+                result = RoshamboResults.scissorsCutPaper
+                message = "Computer wins!"
+                break
+            }
+            break
+        }
+        return (result, message)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +80,31 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func pressedRock(sender: UIButton) {
+        var controller: MatchResultController
+        
+        controller = self.storyboard?.instantiateViewControllerWithIdentifier("matchResult") as! MatchResultController
+        controller.result = self.getMatchResult(RoshamboPlays.ROCK);
+        
+        // Present the view Controller
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func pressedPaper(sender: UIButton) {
+        tempResult = self.getMatchResult(RoshamboPlays.PAPER)
+        self.performSegueWithIdentifier("getResult", sender: self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        println("segue=\(segue.identifier)")
+        if segue.identifier == "getResult"{
+            let controller = segue.destinationViewController as! MatchResultController
+            if let myResult = self.tempResult{
+                controller.result = myResult
+            } else {
+                controller.result = self.getMatchResult(RoshamboPlays.SCISSORS);
+            }
+        }
+    }
 }
 
