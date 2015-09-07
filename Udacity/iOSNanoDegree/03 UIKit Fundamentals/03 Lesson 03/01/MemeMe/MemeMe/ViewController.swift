29bf839840f3c5b068fccd4c634e24c2e33ae931
Luis Antonio <larc63@gmail.com>
2015-09-07 18:13:24 -0500

    Added labels

--- Udacity/iOSNanoDegree/03 UIKit Fundamentals/03 Lesson 03/01/MemeMe/MemeMe/ViewController.swift
+++ Udacity/iOSNanoDegree/03 UIKit Fundamentals/03 Lesson 03/01/MemeMe/MemeMe/ViewController.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var theImage: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
+    var hasClearedTop = false, hasClearedBottom = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let memeTextAttributes = [
+            NSStrokeColorAttributeName : UIColor.whiteColor(),
+            NSForegroundColorAttributeName : UIColor.blackColor(),
+            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName : CGFloat(3.0)
+        ]
+        
+        topText.delegate = self
+        topText.defaultTextAttributes = memeTextAttributes
+        bottomText.delegate = self
+        bottomText.defaultTextAttributes = memeTextAttributes
     }
+    
     override func viewWillAppear(animated: Bool) {
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
@@ -24,6 +40,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField.text == "BOTTOM" && !hasClearedBottom{
+            hasClearedBottom = true
+            textField.text = "";
+        }
+        if textField.text == "TOP" && !hasClearedTop{
+            hasClearedTop = true
+            textField.text = "";
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
     }
 
     @IBAction func pickActionPressed(sender: UIBarButtonItem) {
