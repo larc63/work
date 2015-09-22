8d26cc31269300ec0a4ff137039943448947accf
Luis Antonio <larc63@gmail.com>
2015-09-21 21:18:24 -0500

    Added the RockPaperScissors project

--- Udacity/iOSNanoDegree/03 UIKit Fundamentals/Lesson 05/MYOA/MYOA/ViewController.swift
+++ Udacity/iOSNanoDegree/03 UIKit Fundamentals/Lesson 05/MYOA/MYOA/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem (
+            title: "Start Over",
+            style: UIBarButtonItemStyle.Plain,
+            target: self,
+            action: "startOver")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func startOver() {
+        if let navigationController = self.navigationController {
+            navigationController.popToRootViewControllerAnimated(true)
+        }
+    }
 }
 
