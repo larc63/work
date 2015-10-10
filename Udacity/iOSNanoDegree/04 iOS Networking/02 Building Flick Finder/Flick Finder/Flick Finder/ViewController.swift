d02d990c90442df72e9bd7fbe6228f0eecc10718
Luis Antonio <larc63@gmail.com>
2015-10-10 00:48:17 -0500

    Added Flick Finder project

---
+++ Udacity/iOSNanoDegree/04 iOS Networking/02 Building Flick Finder/Flick Finder/Flick Finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flick Finder
+//
+//  Created by Luis Antonio Rodriguez on 10/10/15.
+//  Copyright © 2015 Luis Antonio Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
