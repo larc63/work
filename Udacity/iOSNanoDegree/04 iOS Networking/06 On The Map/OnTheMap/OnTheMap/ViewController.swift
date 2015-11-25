63b7be1fe2d4b2687b0ceb971f4c7fd3ed8811c2
Luis Antonio <larc63@gmail.com>
2015-11-25 11:39:01 -0800

    Added unit tests to movie manager and added on the map project

---
+++ Udacity/iOSNanoDegree/04 iOS Networking/06 On The Map/OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Luis Antonio Rodriguez on 11/25/15.
+//  Copyright © 2015 Luis Antonio Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
