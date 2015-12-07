f9ab8fb6f966852736456dedd12720d3d3e21e5e
Luis Antonio <larc63@gmail.com>
2015-12-07 10:17:04 -0800

    Added checking controller and outlets/actions

---
+++ Udacity/iOSNanoDegree/04 iOS Networking/06 On The Map/OnTheMap/OnTheMap/CheckinViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CheckinViewController.swift
+//  OnTheMap
+//
+//  Created by Luis Antonio Rodriguez on 12/7/15.
+//  Copyright © 2015 Luis Antonio Rodriguez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CheckinViewController: UIViewController{
+    @IBOutlet weak var searchText: UITextView!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    
+    
+    @IBAction func search(sender: AnyObject) {
+    }
+    
+}
