87ab2294b2ddb77dce29a3167dba71413411d68a
Luis Antonio <larc63@gmail.com>
2015-12-14 12:59:33 -0800

    removed ui tests

--- Udacity/iOSNanoDegree/04 iOS Networking/06 On The Map/OnTheMap/OnTheMapUITests/OnTheMapUITests.swift
+++ Udacity/iOSNanoDegree/04 iOS Networking/06 On The Map/OnTheMap/OnTheMapUITests/OnTheMapUITests.swift
@@ -28,38 +28,9 @@ class OnTheMapUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testLoginHappyPath() {
+    func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
-        let app = XCUIApplication()
-        let usernameTextField = app.textFields["Username"]
-        usernameTextField.tap()
-        usernameTextField.typeText("larc63@gmail.com")
-        
-        let passwordSecureTextField = app.secureTextFields["Password"]
-        UIPasteboard.generalPasteboard().string = "vacaloca"
-        passwordSecureTextField.tap()
-        passwordSecureTextField.doubleTap()
-        app.menuItems["Paste"].tap()
-        app.buttons["Login"].tap()
-        XCTAssertEqual(app.alerts.count, 0, "An alert was shown")
-    }
-    
-    func testLoginNoUserNoPass() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let app = XCUIApplication()
-        let alertString:String = "An error occurred while logging in, make sure that your username and password are correct"
-        app.buttons["Login"].tap()
-        
-        let alert = app.alerts["Error"].collectionViews.buttons["ok"]
-        print("\(alert.staticTexts[alertString])")
-        //        print(alertString)
-        XCTAssertEqual(app.alerts.count, 1, "An alert was not shown")
-        XCTAssertEqual(alert.staticTexts[alertString], alertString, "alert doesn't show the correct string")
-        alert.tap()
-
     }
     
 }
