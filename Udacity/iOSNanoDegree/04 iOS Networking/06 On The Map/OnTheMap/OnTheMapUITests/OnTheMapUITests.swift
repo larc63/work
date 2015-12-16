13996906b8d38171cb0017c1da816f253fcb391f
Luis Antonio <larc63@gmail.com>
2015-12-16 15:52:03 -0800

    re-added UI tests

--- Udacity/iOSNanoDegree/04 iOS Networking/06 On The Map/OnTheMap/OnTheMapUITests/OnTheMapUITests.swift
+++ Udacity/iOSNanoDegree/04 iOS Networking/06 On The Map/OnTheMap/OnTheMapUITests/OnTheMapUITests.swift
@@ -38,23 +38,32 @@ class OnTheMapUITests: XCTestCase {
         
         let passwordSecureTextField = app.secureTextFields["Password"]
         passwordSecureTextField.tap()
-        passwordSecureTextField.typeText("vacaloca")
+        UIPasteboard.generalPasteboard().string = "vacaloca"
+        passwordSecureTextField.doubleTap()
+        app.menuItems["Paste"].tap()
         app.buttons["Login"].tap()
         
         let onthemapMainviewNavigationBar = app.navigationBars["OnTheMap.Mainview"]
+        let buttons = NSPredicate(format: "count == 4")
+        expectationForPredicate(buttons, evaluatedWithObject: onthemapMainviewNavigationBar.buttons, handler: nil)
+        waitForExpectationsWithTimeout(5, handler: nil)
         onthemapMainviewNavigationBar.buttons["MapMarker"].tap()
         
-        let element = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
-        let textView = element.childrenMatchingType(.TextView).elementBoundByIndex(1)
-        textView.tap()
-        textView.typeText("Ensenada, Mexico")
-        app.buttons["Find On Map"].tap()
-        
-        let textView2 = element.childrenMatchingType(.TextView).element
-        textView2.tap()
-        textView2.typeText("www.lavacahacemu.com")
-        app.buttons["Submit"].tap()
-        onthemapMainviewNavigationBar.buttons["Log Out"].tap()
+//        let element = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+////        let geoButton = NSPredicate(format: "count == 1")
+////        expectationForPredicate(geoButton, evaluatedWithObject: element.buttons, handler: nil)
+////        waitForExpectationsWithTimeout(5, handler: nil)
+//        NSThread.sleepForTimeInterval(5)
+//        let textView = element.childrenMatchingType(.TextView).elementBoundByIndex(1)
+//        textView.tap()
+//        textView.typeText("Ensenada, Mexico")
+//        app.buttons["Find On Map"].tap()
+//        
+//        let textView2 = element.childrenMatchingType(.TextView).element
+//        textView2.tap()
+//        textView2.typeText("www.lavacahacemu.com")
+//        app.buttons["Submit"].tap()
+//        onthemapMainviewNavigationBar.buttons["Log Out"].tap()
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
