822dbd38603eee229f2335d05af42b5775cf574f
Luis Antonio <larc63@gmail.com>
2016-02-09 00:30:33 -0600

    Modified PoC

--- CoinHoarder/temp/script.js
+++ CoinHoarder/temp/script.js
@@ -1,18 +1,35 @@
-function ViewModel(){
+function ViewModel() {
     var self = this;
-    self.Tab = function(id, name, text, selected){
+    self.selectedTab = 1;
+
+    self.Tab = function (data) {
         var tab = this;
-        tab.id = ko.observable(id);
-        tab.name = ko.observable(name);
-        tab.text = ko.observable(text);
+        tab.id = ko.observable(data.id);
+        tab.name = ko.observable(data.name);
+        tab.text = ko.observable(data.text);
+
+        function reqListener() {
+            tab.text(this.responseText);
+        }
+
+        if (data.url) {
+            var oReq = new XMLHttpRequest();
+            oReq.addEventListener("load", reqListener);
+            oReq.open("GET", data.url);
+            oReq.send();
+        }
         return tab;
     };
+
+
     self.selectedTab = ko.observable(1);
     self.tabs = new Array();
-    self.tabs.push(new self.Tab(1, 'Tab 1', 'Wecome to Tab #1!'));
-    self.tabs.push(new self.Tab(2, 'Tab 2', 'This is Tab 2...'));
-    self.tabs.push(new self.Tab(3, 'Tab 3', 'I\'m tab 3'));
-    self.tabs.push(new self.Tab(4, 'Tab 4', 'Hello World!'));
+    //(id, name, url, text, selected
+    self.tabs.push(new self.Tab({id: 1, name: 'Tab 1', url: 'tab01.html', text: 'Wecome to Tab #1!'}));
+    self.tabs.push(new self.Tab({id: 2, name: 'Tab 2', url: undefined, text: 'This is Tab 2...'}));
+    self.tabs.push(new self.Tab({id: 3, name: 'Tab 3', url: undefined, text: 'I\'m tab 3'}));
+    self.tabs.push(new self.Tab({id: 4, name: 'Tab 4', url: undefined, text: 'Hello World!'}));
+
     return self;
 }
 
