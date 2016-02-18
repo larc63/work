ea5376c04ad89a1afcaed947daa2e9154dda176e
Luis Antonio <larc63@gmail.com>
2016-02-17 21:26:09 -0600

    Added recipe randomizer

---
+++ Udacity/iOSNanoDegree/RecipeRandomizer/RecipeRandomizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecipeRandomizer
+//
+//  Created by Luis Antonio Rodriguez on 2/17/16.
+//  Copyright © 2016 Luis Antonio Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Udacity/iOSNanoDegree/RecipeRandomizer/RecipeRandomizerTests/RecipeRandomizerTests.swift
@@ -0,0 +1,36 @@
+//
+//  RecipeRandomizerTests.swift
+//  RecipeRandomizerTests
+//
+//  Created by Luis Antonio Rodriguez on 2/17/16.
+//  Copyright © 2016 Luis Antonio Rodriguez. All rights reserved.
+//
+
+import XCTest
+@testable import RecipeRandomizer
+
+class RecipeRandomizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
